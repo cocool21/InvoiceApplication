7f394af06bcaceb404080afecbc4cbce07babcf9
Chenye Yin <elizebethyin@gmail.com>
2016-03-04 17:03:01 -0500

    Oh

---
+++ src/InvoiceApplication.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class InvoiceApplication {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter tax rate followed by prices");
+
+	}
+
+}
